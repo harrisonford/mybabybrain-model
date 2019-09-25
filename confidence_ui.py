912548c5dd80ba7dbef9baa77f1ae9040802b722
harrisonford <kristofher@ug.uchile.cl>
2019-09-25 14:43:42 -0300

    clean-up for confidence code

--- confidence_ui.py
+++ confidence_ui.py
@@ -1,5 +1,6 @@
 import poseModels
 import os
+import sys
 from imageio import imread
 import numpy as np
 import tensorflow as tf
@@ -7,6 +8,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as grid
 import cv2
 import warnings
+import shutil
 plt.rcParams.update({'font.size': 10})
 
 
@@ -123,166 +125,51 @@ def save_frame_result(output_file, image_matrix, heatmaps, confidences, part_nam
     plt.close()
 
 
-def main(subsample=1, n_stop=24000,
-         path='/home/babybrain/Escritorio/frames_seg_000790/',
-         outpath='/home/babybrain/Escritorio/frames_seg_000790_results/'):
-    # we can subsample files in case we want less but fast results, a value of 1 implies no subsample
-    file_names = sorted(os.listdir(path))
-    file_names = [a_frame for num, a_frame in enumerate(file_names) if num % subsample == 0 and num <= n_stop]
-    full_paths = [path + a_frame for a_frame in file_names]
+def main(input_video, output_video, nstop=999999):
 
-    # run models
-    model_human = poseModels.HumanPoseModel(input_list=full_paths)
-    model_human.run_model(verbose=True)
-    confidences_human = model_human.calculate_confidence()
+    # create the human pose model
+    model = poseModels.HumanPoseModel()
 
-    tf.reset_default_graph()
-    model_pose = poseModels.PoseEstimationModel(input_list=full_paths)
-    model_pose.run_model(verbose=True)
-    confidences_pose = model_pose.calculate_confidence()
-
-    # for each input/output/confidence trio we'll make a cool image
+    # extract each frame
+    vidcap = cv2.VideoCapture(input_video)
+    success, image = vidcap.read()
+    # for each input/output/confidence trio we'll make a cool image, as output we'll use a _temp folder
     # we'll show a heatmap overlay on the original frame on left side plus confidence bars on the right side
-    heatmaps_human = []
-    heatmaps_pose = []
-    for index, a_file in enumerate(file_names):
+    temp_path = './_temp'
+    os.mkdir(temp_path)
+    video_dimensions = (image.shape[1], image.shape[0])
+    fps = 240
+    video = cv2.VideoWriter(output_video, cv2.VideoWriter_fourcc(*'MJPG'), fps, video_dimensions)
+    while success:
+        # apply model to frame
+        an_output = model._run_model_once(image)
+        model.outputs.append(an_output)
+        # calculate heat-map
+        a_heatmap_set = model.make_heatmaps_once(an_output)
+        # calculate confidence set
+        a_confidence_set = model.calculate_confidence_once(an_output)
 
-        # read image
-        image = imread(full_paths[index])
+        this_frame = vidcap.get(0)
+        if this_frame > nstop:
+            break
+        print("saving frame {}".format(this_frame))
+        temp_out = "{}/{}.png".format(temp_path, this_frame)
+        save_frame_result(temp_out, image, a_heatmap_set, a_confidence_set, model.joint_names, 'HumanPose')
+        success, image = vidcap.read()
 
-        # calculate part heatmaps
-        heatmaps_human.append(model_human.make_heatmaps_once(model_human.outputs[index]))
-        heatmaps_pose.append(model_pose.make_heatmaps_once(model_pose.outputs[index]))
-
-        # confidence for output
-        conf_human = confidences_human[index]
-        conf_pose = confidences_pose[index]
-
-        # save result frames
-        outpath_human = outpath + 'HumanPose/' + a_file
-        outpath_pose = outpath + 'PoseEst/' + a_file
-        outpath_dual = outpath + 'Both/' + a_file
-
-        print("saving frame {}".format(a_file))
-        save_frame_result(outpath_human, image, heatmaps_human[index], conf_human,
-                          model_human.joint_names, model_human.model_name)
-        save_frame_result(outpath_pose, image, heatmaps_pose[index], conf_pose,
-                          model_human.joint_names, model_pose.model_name)
-        save_frame_result_dual(outpath_dual, image, [heatmaps_human[index], heatmaps_pose[index]],
-                               [conf_human, conf_pose], model_human.joint_names,
-                               [model_human.model_name, model_pose.model_name])
-
-    # plot final image
-    fig = plt.figure(figsize=(12, 8))
-    outer_canvas = grid.GridSpec(1, 2)
-
-    # plot a frame on the left: mean heatmaps
-    n_parts = len(heatmaps_human[0])  # check how many sub heatmaps we have
-    cum_heatmap_human = [np.zeros((3, 3)) for _ in range(n_parts)]  # empty matrices
-    cum_heatmap_pose = [np.zeros((3, 3)) for _ in range(n_parts)]
-    for human_heatmap, pose_heatmap in zip(heatmaps_human, heatmaps_pose):
-        for index, (human_submap, pose_submap) in enumerate(zip(human_heatmap, pose_heatmap)):
-            # resize cumulative heatmap to new addition if needed
-            cum_heatmap_human[index], human_submap = extend_matrices(cum_heatmap_human[index], human_submap)
-            cum_heatmap_pose[index], pose_submap = extend_matrices(cum_heatmap_pose[index], pose_submap)
-            cum_heatmap_human[index] += human_submap
-            cum_heatmap_pose[index] += pose_submap
-
-    used_frame = model_human.input_list[-1]  # last frame, whatever you want to show
-    image = imread(used_frame)
-    # plot heatmaps in first grid
-    left_canvas = grid.GridSpecFromSubplotSpec(4, 2, subplot_spec=outer_canvas[0])
-    for num, a_heatmap in enumerate(cum_heatmap_human):
-        ax = plt.Subplot(fig, left_canvas[num])
-        ax.set_title(model_human.joint_names[num])
-        ax.axis('off')
-        ax.imshow(image, interpolation='bilinear')
-        if np.any(a_heatmap):
-            ax.imshow(a_heatmap, alpha=0.5, cmap='jet', interpolation='bilinear')
-        fig.add_subplot(ax)
-
-    # plot average confidences to the right
-    right_canvas = grid.GridSpecFromSubplotSpec(1, 1, subplot_spec=outer_canvas[1])
-    ax = plt.Subplot(fig, right_canvas[0])
-
-    # human
-    confidences = np.absolute(np.array(confidences_human))
-    confidences_mean = np.nanmean(confidences, axis=0)
-    confidences_std = np.nanstd(confidences, axis=0)
-    bar_width = 0.35
-    opacity = 0.4
-    error_config = {'ecolor': '0.3'}
-    n_groups = len(confidences_mean)
-    index = np.arange(n_groups)
-    ax.bar(index, confidences_mean, bar_width, alpha=opacity, color='b', yerr=confidences_std,
-           error_kw=error_config, label='Human-Pose')
-    # pose-est
-    confidences = np.absolute(np.array(confidences_pose))
-    confidences_mean = np.nanmean(confidences, axis=0)
-    confidences_std = np.nanstd(confidences, axis=0)
-    ax.bar(index + bar_width, confidences_mean, bar_width, alpha=opacity, color='r', yerr=confidences_std,
-           error_kw=error_config, label='Pose-Est')
-
-    ax.set_ylim(0, 1)
-    ax.set_xlabel('Body-Part')
-    ax.set_ylabel('Confidence Value')
-    ax.set_title('Mean Confidence Values in one video')
-    ax.set_xticks(index + bar_width/2)
-    ax.set_xticklabels(model_human.model_config.all_joints_names)
-    ax.legend()
-
-    fig.add_subplot(ax)
-    plt.savefig("{}{}.png".format(outpath, "0"), format='png')
-    plt.close()
-
-    # finally create a sample video from all frames for each model
-    average_frames = 300
-    # human
-    frame_list = [outpath + 'HumanPose/' + a_file for a_file in file_names]
-    average_frame = cv2.imread(outpath + '0.png')
-    im_dim = (average_frame.shape[1], average_frame.shape[0])
-    video = cv2.VideoWriter('{}sample_video_human.avi'.format(outpath),
-                            cv2.VideoWriter_fourcc(*'MJPG'), 60, im_dim)
-    for a_frame in frame_list:
-        frame = cv2.imread(a_frame)
+        frame = cv2.imread(temp_out)
         video.write(frame)
-    # also write the average_frame many times
-    for i in range(average_frames):
-        video.write(average_frame)
-    # release writer
+
+    # clean up
     cv2.destroyAllWindows()
     video.release()
-
-    # pose
-    frame_list = [outpath + 'PoseEst/' + a_file for a_file in file_names]
-    im_dim = (average_frame.shape[1], average_frame.shape[0])
-    video = cv2.VideoWriter('{}sample_video_pose.avi'.format(outpath),
-                            cv2.VideoWriter_fourcc(*'MJPG'), 60, im_dim)
-    for a_frame in frame_list:
-        frame = cv2.imread(a_frame)
-        video.write(frame)
-    # also write the average_frame many times
-    for i in range(average_frames):
-        video.write(average_frame)
-    # release writer
-    cv2.destroyAllWindows()
-    video.release()
-
-    # both
-    frame_list = [outpath + 'Both/' + a_file for a_file in file_names]
-    im_dim = (average_frame.shape[1], average_frame.shape[0])
-    video = cv2.VideoWriter('{}sample_video_both.avi'.format(outpath),
-                            cv2.VideoWriter_fourcc(*'MJPG'), 60, im_dim)
-    for a_frame in frame_list:
-        frame = cv2.imread(a_frame)
-        video.write(frame)
-    # also write the average_frame many times
-    for i in range(average_frames):
-        video.write(average_frame)
-    # release writer
-    cv2.destroyAllWindows()
-    video.release()
+    shutil.rmtree(temp_path)
 
 
 if __name__ == '__main__':
-    main(subsample=1, n_stop=1)
+    # parse arguments input and output video
+    # input_path = str(sys.argv[1])
+    # output_path = str(sys.argv[2])
+    input_path = '/home/harrisonford/Videos/babybrain/000345.MP4'
+    output_path = './sample.avi'
+    main(input_path, output_path, nstop=100)
