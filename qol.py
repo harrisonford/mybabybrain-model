6d237abea0f2a85303fe8350515ee45c792e4c7f
harrisonford <kristofher@ug.uchile.cl>
2019-12-31 11:34:34 -0300

    developed random forest classifier

--- qol.py
+++ qol.py
@@ -2,6 +2,8 @@
 import cv2
 import numpy as np
 import json
+import matplotlib.pyplot as plt
+from sklearn.metrics import precision_score, recall_score, roc_auc_score, roc_curve
 
 
 # A silly function to set a default value if not in kwargs
@@ -173,3 +175,44 @@ def compute_error(dt_path, gt_path, normalize=None, threshold=0.250):
                 distances.append(np.hypot(delta_x, delta_y)*norm_factor)
                 visible.append(gt_visible)
     return distances, visible
+
+
+# compare machine learning model to baseline performance with a ROC
+def evaluate_model(predictions, probs, train_predictions, train_probs, train_labels, test_labels, output=None):
+    baseline = {}
+    baseline['recall'] = recall_score(test_labels, [1 for _ in range(len(test_labels))])
+    baseline['precision'] = precision_score(test_labels, [1 for _ in range(len(test_labels))])
+    baseline['roc'] = 0.5
+
+    results = {}
+    results['recall'] = recall_score(test_labels, predictions)
+    results['precision'] = precision_score(test_labels, predictions)
+    results['roc'] = roc_auc_score(test_labels, probs)
+
+    train_results = {}
+    train_results['recall'] = recall_score(train_labels, train_predictions)
+    train_results['precision'] = precision_score(train_labels, train_predictions)
+    train_results['roc'] = roc_auc_score(train_labels, train_probs)
+
+    for metric in ['recall', 'precision', 'roc']:
+        print(
+            f'{metric.capitalize()} Baseline: {round(baseline[metric], 2)}'
+            f' Test: {round(results[metric], 2)} Train: {round(train_results[metric], 2)}')
+
+    # Calculate false positive rates and true positive rates
+    base_fpr, base_tpr, _ = roc_curve(test_labels, [1 for _ in range(len(test_labels))])
+    model_fpr, model_tpr, _ = roc_curve(test_labels, probs)
+
+    plt.figure(figsize=(8, 6))
+    plt.rcParams['font.size'] = 16
+
+    # Plot both curves
+    plt.plot(base_fpr, base_tpr, 'b', label='baseline')
+    plt.plot(model_fpr, model_tpr, 'r', label='model')
+    plt.legend()
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curves')
+
+    if output:
+        plt.savefig(output)
