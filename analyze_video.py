f30b5366c08d1acf22cc8d5031b49fb318ba304e
harrisonford <kristofher@ug.uchile.cl>
2020-01-20 10:22:30 -0300

    added two pickle models for normal and gm class

--- analyze_video.py
+++ analyze_video.py
@@ -5,6 +5,7 @@ import cv2
 import json
 import numpy as np
 import pandas as pd
+import pickle
 
 
 def save_as_json(serializable_data, file_path):
@@ -20,7 +21,49 @@ def classify_subject_dummy(total_frames):
     durations = [total_duration*0.1, total_duration*0.15]
     dummy_intervals = [[starts[0], starts[0]+durations[0]],
                        [starts[1], starts[1]+durations[1]]]
-    return dummy_intervals
+    return pd.DataFrame(dummy_intervals, columns=['T0', 'T1'])
+
+
+def classify_subject_confidences(my_pickle_model, _confidence_list, _frame_list,
+                                 window_size_seconds=10, fps=240, fskip=30):
+    if not my_pickle_model:  # no model, we just use a dummy
+        return classify_subject_dummy(np.max(_frame_list))
+
+    window_size_data = int(window_size_seconds * fps / fskip)  # at 240fps and 30 fskip we have 8 data per second
+
+    # calculate predictions
+    # first we remove possible nans from data
+    clean_confidence_list = [a_sample for a_sample in _confidence_list if not np.any(np.isnan(a_sample))]
+    predictions = my_pickle_model.predict(clean_confidence_list)
+
+    # compress into abnormal windows
+    # we will store the windows into a simple format t0, t1 for desired windows
+    compressed_windows = []
+    for index in range(0, len(predictions), window_size_data):
+        sub_sample = predictions[index:index + window_size_data - 1]
+        score = np.nanmean(sub_sample)
+        if score < 0.5:  # then we have an abnormal case
+            t0 = _frame_list[index] / fps
+            t1 = _frame_list[index] / fps + window_size_seconds
+            compressed_windows.append([t0, t1])
+
+    # now we extend windows they're next to each other
+    compressed_windows_extended = []
+    stored_t0 = None
+    stored_t1 = None
+    if compressed_windows:
+        stored_t0, stored_t1 = compressed_windows[0]
+
+    for index, (t0, t1) in enumerate(compressed_windows):
+        # we store the values if not connected to next window or last window
+        if stored_t1 - t0 > window_size_seconds or index == len(compressed_windows) - 1:
+            compressed_windows_extended.append([stored_t0, stored_t1])
+            stored_t0 = t0
+        else:  # we keep compressing windows
+            stored_t1 = t1
+
+    # now we make a DataFrame to save T0 and T1 and return it
+    return pd.DataFrame(compressed_windows_extended, columns=['T0', 'T1'])
 
 
 if __name__ == '__main__':
@@ -28,15 +71,18 @@ if __name__ == '__main__':
     ap.add_argument("--in", type=str, default=None, help="full path to input video file")
     ap.add_argument("--out", type=str, default=None, help="full path to output video file")
     ap.add_argument("--fskip", type=int, default=30, help="set frame jump for each processed")
+    ap.add_argument("--model", type=str, default=None, help="full path to final classification model")
     args = vars(ap.parse_args())
 
     file_in = args['in']
     file_out = args['out']
     fskip = args['fskip']
+    model_path = args['model']
 
-    # prepare model
+    # prepare models
     tf.reset_default_graph()
     model = HumanPoseModel()
+    class_model = pickle.load(open(model_path, 'rb'))
 
     # container to save confidence values
     confidences = []
@@ -68,12 +114,11 @@ if __name__ == '__main__':
     confidence_list = np.array(confidences).tolist()
     data = dict(file_name=file_in, model_name=model.model_name, processed_frames=frame_list,
                 confidences=confidence_list)
-    # TODO: Working on final step classification will attach a dummy model for now
-    save_as_json(data, file_out + '.json')
+    # save the model data just in case
+    # save_as_json(data, file_out + '.json')
 
     # classify subject data
-    intervals = classify_subject_dummy(frame_count)
+    intervals_df = classify_subject_confidences(class_model, confidence_list, frame_list)
 
     # save intervals as DataFrame
-    df = pd.DataFrame(intervals, columns=['T0', 'Tf'])
-    df.to_csv(file_out + '.csv')
+    intervals_df.to_csv(file_out + '.csv')
